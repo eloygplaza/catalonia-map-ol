e638fed88bb202eb938c70d38e9a7c61bed57cf2
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-11-22 17:42:51 +0100

    logic added

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function init() {
     view: new ol.View({
       center: [188527.0981312341, 5145259.000081155],
       zoom: 1,
-      extent: [-17220, 4889170, 435926, 5396266],
+      extent: [-20220, 4789170, 435926, 5496266],
     }),
     layers: [
       new ol.layer.Tile({
@@ -14,12 +14,39 @@ function init() {
       }),
     ],
     target: "openlayers-map",
-    //controls: ol.control.defaults({ attribution: false }).extend(mapControls),
   });
 
   // Catalonia Ctiies GeoJSON
   const catCitiesStyle = (feature) => {
-    console.log(feature);
+    let cityID = feature.get("id");
+    let cityIDString = cityID.toString();
+    const styles = [
+      new ol.style.Style({
+        image: new ol.style.Circle({
+          fill: new ol.style.Fill({
+            color: [77, 219, 105, 0.6],
+          }),
+          stroke: new ol.style.Stroke({
+            color: [6, 125, 34, 1],
+            width: 2,
+          }),
+          radius: 12,
+        }),
+        text: new ol.style.Text({
+          text: cityIDString,
+          scale: 1.5,
+          fill: new ol.style.Fill({
+            color: [232, 26, 26, 1],
+          }),
+          stroke: new ol.style.Stroke({
+            color: [232, 26, 26, 1],
+            with: 0.3,
+          }),
+        }),
+      }),
+    ];
+
+    return styles;
   };
 
   const cataloniaCities = new ol.layer.Vector({
@@ -30,4 +57,33 @@ function init() {
     style: catCitiesStyle,
   });
   map.addLayer(cataloniaCities);
+
+  // Map Features Click Logic
+  const navElements = document.querySelector(".column-navigation");
+  const cityNameElement = document.getElementById("cityname");
+  const cityImageElement = document.getElementById("cityimage");
+  const mapView = map.getView();
+
+  map.on("singleclick", (evt) => {
+    map.forEachFeatureAtPixel(evt.pixel, (feature, layer) => {
+      let featureName = feature.get("name");
+      let navElement = navElements.children.namedItem(featureName);
+      mainLogic(feature, navElement);
+      console.log(feature);
+      console.log(navElement);
+    });
+  });
+
+  const mainLogic = (feature, clickedAnchorElement) => {
+    // Re-assign active class to the clicked element
+    let currentActiveStyledElement = document.querySelector(".active");
+    currentActiveStyledElement.className =
+      currentActiveStyledElement.className.replace("active", "");
+    clickedAnchorElement.className = "active";
+
+    // change the view based on the feature
+    let featureCoordinates = feature.get("geometry").getCoordinates();
+    mapView.animate({ center: featureCoordinates }, { zoom: 5 });
+    console.log(featureCoordinates);
+  };
 }
