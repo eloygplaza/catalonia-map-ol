f39d8e4402df8e5271ee64804690a8f07e8bd2b9
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-11-24 09:59:11 +0100

    added all logic

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ window.onload = init;
 
 function init() {
   // New Map
+  const catCenterCoordinates = [188527.0981312341, 5145259.000081155];
   const map = new ol.Map({
     view: new ol.View({
-      center: [188527.0981312341, 5145259.000081155],
-      zoom: 1,
-      extent: [-20220, 4789170, 435926, 5496266],
+      center: catCenterCoordinates,
+      zoom: 7,
+      //extent: [-20220, 4789170, 435926, 5496266],
     }),
     layers: [
       new ol.layer.Tile({
@@ -16,7 +17,7 @@ function init() {
     target: "openlayers-map",
   });
 
-  // Catalonia Ctiies GeoJSON
+  // Catalonia Cities GeoJSON
   const catCitiesStyle = (feature) => {
     let cityID = feature.get("id");
     let cityIDString = cityID.toString();
@@ -49,14 +50,46 @@ function init() {
     return styles;
   };
 
-  const cataloniaCities = new ol.layer.Vector({
+  const styleForSelect = (feature) => {
+    let cityID = feature.get("id");
+    let cityIDString = cityID.toString();
+    const styles = [
+      new ol.style.Style({
+        image: new ol.style.Circle({
+          fill: new ol.style.Fill({
+            color: [214, 26, 105, 0.5],
+          }),
+          stroke: new ol.style.Stroke({
+            color: [6, 125, 34, 1],
+            width: 2,
+          }),
+          radius: 12,
+        }),
+        text: new ol.style.Text({
+          text: cityIDString,
+          scale: 1.5,
+          fill: new ol.style.Fill({
+            color: [87, 9, 9, 1],
+          }),
+          stroke: new ol.style.Stroke({
+            color: [87, 9, 9, 1],
+            with: 0.5,
+          }),
+        }),
+      }),
+    ];
+
+    return styles;
+  };
+
+  const cataloniaCitiesLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
       format: new ol.format.GeoJSON(),
       url: "./data/catalonia_cities.geojson",
     }),
     style: catCitiesStyle,
   });
-  map.addLayer(cataloniaCities);
+  map.addLayer(cataloniaCitiesLayer);
 
   // Map Features Click Logic
   const navElements = document.querySelector(".column-navigation");
@@ -81,9 +114,85 @@ function init() {
       currentActiveStyledElement.className.replace("active", "");
     clickedAnchorElement.className = "active";
 
-    // change the view based on the feature
-    let featureCoordinates = feature.get("geometry").getCoordinates();
-    mapView.animate({ center: featureCoordinates }, { zoom: 5 });
-    console.log(featureCoordinates);
+    // Default Style for all features
+    let cataloniaCitiesFeatures = cataloniaCitiesLayer
+      .getSource()
+      .getFeatures();
+    cataloniaCitiesFeatures.forEach((feature) => {
+      feature.setStyle(catCitiesStyle);
+      //console.log(feature);
+    });
+    feature.setStyle(styleForSelect);
+
+    console.log(clickedAnchorElement);
+
+    // Home Element : Change content in the menu to HOME
+    if (clickedAnchorElement.id === "Home") {
+      mapView.animate({ center: catCenterCoordinates }, { zoom: 7 });
+      cityNameElement.innerHTML =
+        "Welcome to Catalonia Capital Cities Tour Map";
+      cityImageElement.setAttribute("src", "./data/images/catalonia_flag.jpg");
+    }
+
+    // change view and content based on the feature
+    else {
+      feature.setStyle(styleForSelect);
+      let featureCoordinates = feature.get("geometry").getCoordinates();
+      mapView.animate({ center: featureCoordinates }, { zoom: 9 });
+      let featureName = feature.get("name");
+      let featureImage = feature.get("image");
+      cityNameElement.innerHTML = "Name of the city: " + featureName;
+      cityImageElement.setAttribute(
+        "src",
+        "./data/images/" + featureImage + ".jpg"
+      );
+    }
   };
+
+  // Navigation Button Logic
+  const anchorNavElements = document.querySelectorAll(".column-navigation > a");
+  for (let anchorNavElement of anchorNavElements) {
+    anchorNavElement.addEventListener("click", (e) => {
+      let clickedAnchorElement = e.currentTarget;
+      let clickedAnchorElementID = clickedAnchorElement.id;
+      console.log(clickedAnchorElementID);
+      let cataloniaCitiesFeatures = cataloniaCitiesLayer
+        .getSource()
+        .getFeatures();
+      cataloniaCitiesFeatures.forEach((feature) => {
+        let featureCityName = feature.get("name");
+        if (clickedAnchorElementID === featureCityName) {
+          mainLogic(feature, clickedAnchorElement);
+        }
+        //console.log(feature.get("name"));
+        // Home Navigation Case
+        if (clickedAnchorElementID === "Home") {
+          mainLogic(feature, clickedAnchorElement);
+        }
+      });
+    });
+  }
+
+  // Features Hover Logic
+  const popoverTextElement = document.getElementById("popover-text");
+  const popoverTextLayer = new ol.Overlay({
+    element: popoverTextElement,
+    positioning: "bottom-center",
+    stopEvent: false,
+  });
+  map.addOverlay(popoverTextLayer);
+
+  map.on("pointermove", (evt) => {
+    let isFeatureAtPixel = map.hasFeatureAtPixel(evt.pixel);
+    if (isFeatureAtPixel) {
+      let featureAtPixel = map.getFeaturesAtPixel(evt.pixel);
+      let featureName = featureAtPixel[0].get("name");
+      popoverTextLayer.setPosition(evt.coordinate);
+      popoverTextElement.innerHTML = featureName;
+      map.getViewport().style.cursor = "pointer";
+    } else {
+      popoverTextLayer.setPosition(undefined);
+      map.getViewport().style.cursor = "";
+    }
+  });
 }
